00ab6c97d9ed68187104c8b5e5905bf8016327d5
Rizart Dokollari <r.dokollari@gmail.com>
2024-08-23 21:55:46 +0100

    chore: Initialise project.

---
+++ NoNotifySneakDamage/Harmony/Main.cs
@@ -0,0 +1,15 @@
+﻿namespace NoNotifySneakDamage.Harmony
+{
+    public class Main
+    {
+        public class Init : IModApi
+        {
+            public void InitMod(Mod modInstance)
+            {
+                const string id = "uk.co.rdok.7daystodie.mods.no_notify_sneak_damage";
+                var harmony = new HarmonyLib.Harmony(id);
+                harmony.PatchAll();
+            }
+        }
+    }
+}
